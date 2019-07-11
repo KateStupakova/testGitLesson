984a989947ff80dbfab11f6e70488de6f069ec6a
Zver <Zver@192.168.10.170>
2019-07-11 19:17:49 +0300

    https://github.com/KateStupakova/work5.git

--- work5/src/work5/work5.java
+++ work5/src/work5/work5.java
@@ -5,26 +5,38 @@ import java.util.Arrays;
 public class work5 {
 
 	public static void main(String[] args) {
-		 {
-		        
+		{
+	        
 
-		        Scanner input = new Scanner(System.in);
-		        System.out.println("Заполните массив");
-		        int mas[] = new int[10];
-		        for (int i = 0; i < mas.length; i++) {
-		            mas[i] = input.nextInt();
-		        }
+	        Scanner input = new Scanner(System.in);
+	        System.out.println("Заполните массив");
+	        int mas[] = new int[10];//создаем  массив размером 10
+	        for (int i = 0; i < mas.length; i++) {
+	            mas[i] = input.nextInt();// заполняем массив с клавиатуры
+	        }
+	 
+	 
+	        System.out.println("Заполненый массив");
+	        for (int i=0;i < mas.length;i++){
+	            System.out.println(maxArr(mas));// Выводим заполненый массив на экран
+	        }
+	        System.out.println();
 		 
 		 
-		        System.out.println("Заполненый массив");
-		        for (int i=0;i < mas.length;i++){
-		            System.out.println(maxArr(mas));
-		        }
-		        System.out.println();
-			 
-			 
 
-		    }
+	    }
+}
+	 
+	 public static int maxArr(int[] array) {
+	        int max = 0;
+	        for (int i = 0; i < array.length; i++) {
+	            if (max < array[i]) {
+	                max = array[i];
+	            }
+	        }
+
+	        return max;
+
 	}
 
 }
