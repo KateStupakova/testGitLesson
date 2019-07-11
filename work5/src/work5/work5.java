2bb8b59ab7098c240e92a8d757630e5e1c3c7c18
Zver <Zver@192.168.10.170>
2019-07-11 19:02:58 +0300

    https://github.com/KateStupakova/work5.git

--- work5/src/work5/work5.java
+++ work5/src/work5/work5.java
@@ -1,9 +1,30 @@
 package work5;
 
+import java.util.Scanner;
+
 public class work5 {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		 {
+		        
+
+		        Scanner input = new Scanner(System.in);
+		        System.out.println("Заполните массив");
+		        int mas[] = new int[10];
+		        for (int i = 0; i < mas.length; i++) {
+		            mas[i] = input.nextInt();
+		        }
+		 
+		 
+		        System.out.println("Заполненый массив");
+		        for (int i=0;i < mas.length;i++){
+		            System.out.println(maxArr(mas));
+		        }
+		        System.out.println();
+			 
+			 
+
+		    }
 	}
 
 }
