6dc7887ab96aab920b57c0647b2246449c9f31fb
Zver <Zver@192.168.10.170>
2019-07-11 21:13:42 +0300

    https://github.com/KateStupakova/work5.git

--- work5/src/work5/work5.java
+++ work5/src/work5/work5.java
@@ -1,42 +1,29 @@
 package work5;
-
 import java.util.Scanner;
 import java.util.Arrays;
 public class work5 {
 
 	public static void main(String[] args) {
-		{
+		
 	        
+		 {
+		        
 
-	        Scanner input = new Scanner(System.in);
-	        System.out.println("Заполните массив");
-	        int mas[] = new int[10];//создаем  массив размером 10
-	        for (int i = 0; i < mas.length; i++) {
-	            mas[i] = input.nextInt();// заполняем массив с клавиатуры
-	        }
-	 
-	 
-	        System.out.println("Заполненый массив");
-	        for (int i=0;i < mas.length;i++){
-	            System.out.println(maxArr(mas));// Выводим заполненый массив на экран
-	        }
-	        System.out.println();
+		        Scanner input = new Scanner(System.in);
+		        System.out.println("Заполните массив");
+		        int mas[] = new int[10];//создаем  массив размером 10
+		        for (int i = 0; i < mas.length; i++) {
+		            mas[i] = input.nextInt();// заполняем массив с клавиатуры
+		        }
 		 
 		 
-
-	    }
-}
-	 
-	 public static int maxArr(int[] array) {
-	        int max = 0;
-	        for (int i = 0; i < array.length; i++) {
-	            if (max < array[i]) {
-	                max = array[i];
-	            }
-	        }
-
-	        return max;
-
+		        System.out.println("Заполненый массив");
+		        for (int i=0;i < mas.length;i++){
+		            System.out.println(maxArr(mas));// Выводим заполненый массив на экран
+		        }
+		        System.out.println();
+			 
+	      
 	}
 
 }
